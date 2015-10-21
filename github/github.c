6c8362449e3ee77350817b97ce343a3becce5822
mengxl <mengxl2020.gmail.com>
2015-10-21 10:14:30 +0800

    updata

--- github/github.c
+++ github/github.c
@@ -1,30 +1,15 @@
 
-//pull
+//init
 git init
 git remote add origin git@github.com:yujue86/xxx.git
+
+//pull
 git pull origin master
 
 //push 
-git init
 git add .	//add all file
 git commit -m 'xxx'		//提交更新，并描述本次更新为“xxx”
-git remote add origin git@github.com:yujue86/xxx.git
 git push origin master
 
 //rm origin
 git remote rm origin
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-mengxl2020@gmail.com
